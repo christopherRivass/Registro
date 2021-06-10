cc895e42faf3a4fbfb6dc602c03c850805ff123e
Christopher Rivas <christopherrivas422@gmail.com>
2021-06-10 15:27:06 -0600

    problemas con mysql

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-	echo 'Current PHP version' .phpversion()
+$server='localhost';
+$user='chris';
+$password='Tsukujomy[19]';
+$bd='Alumnos';
+
+$conn = mysqli_connect($server, $user, $password, $bd);
+if(!$conn){
+    echo 'Parece que fallo tu base de datos';
+}else{
+    echo 'Todo bien';
+}
 ?>
