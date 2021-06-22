feb2e87573a7398e8cb0075336d47c771dd89f72
christopherRivass <christopherrivas422@gmail.com>
2021-06-22 17:52:23 -0600

    arreglando variables

--- conct.php
+++ conct.php
@@ -4,16 +4,11 @@ $contra='Tsukujomy[19]';
 $servidor='localhost';
 $baseDedatos='Alumnos';
 
-$conn = mysqli_connect($servidor,$user,$contra,"Alumnos");
+$conn = mysqli_connect($servidor,$user,$contra,$baseDedatos);
 
 if (!$servidor) {
     echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
     echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
-    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
-    exit;
 }
 
 echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
-echo "Información del host: " . mysqli_get_host_info($servidor) . PHP_EOL;
-
-mysqli_close($enlace);
