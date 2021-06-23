aa368f3260108647b86fc8965c07eeb089265187
christopherRivass <christopherrivas422@gmail.com>
2021-06-22 18:26:57 -0600

    ya funviona la bbdd

--- conct.php
+++ conct.php
@@ -1,14 +1,15 @@
 <?php
+echo 'hola';
 $user='chris';
 $contra='Tsukujomy[19]';
 $servidor='localhost';
 $baseDedatos='Alumnos';
 
-$conn = mysqli_connect($servidor,$user,$contra,$baseDedatos);
-
-if (!$servidor) {
+$conn=mysqli_connect($servidor,$user,$contra);
+if (!$conn) {
     echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
     echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+}else{
+    echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
 }
 
-echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
