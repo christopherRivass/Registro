8988be86be12b45f47205aa42752069783e597fa
christopherRivass <christopherrivas422@gmail.com>
2021-06-29 15:30:45 -0600

    i stared with style, after i will with bbdd and others

--- index.php
+++ index.php
@@ -9,20 +9,20 @@
 		<div>
 			<form action="save/saveInfo.php" method="POST">
 				<h4>Put your DNI</h4>
-				<input type="text" name="DNI" placeholder="000012122343">
+				<input type="text" name="DNI" class="DNI" placeholder="000012122343">
 				<h4>Tell me your name</h4>
-				<input type="text" name="itName" placeholder="Francisco Rodriguex">
+				<input type="text" name="itName" class="itName" placeholder="Francisco Rodriguex">
 				<h4>How old are you</h4>
-				<input type="number" name="years" placeholder="18">
+				<input type="number" name="years" class="years" placeholder="18">
 				<h4>What's your grade calssroom</h4>
-				<select name="curso" size="1">
+				<select name="curso" class="selectHTML" size="1">
 					<option>BCH</option>
 					<option>BTPCF</option>
 					<option>BTPI</option>
 					<option>BTPR</option>
 				</select>
 				<h4> Your calss day</h4>
-				<input type="text" name="day" placeholder="Mornin,Night">
+				<input type="text" name="day" class="day" placeholder="Mornin,Night">
 				<input type="submit" name="saveInfo"></input>
 			</form>
 		</div>
